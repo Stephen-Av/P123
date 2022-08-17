e9fa747945eabcecf44c35292f08cc80df54e817
Stephen-Av <94113921+Stephen-Av@users.noreply.github.com>
2022-08-17 18:43:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftwristX = 0;
+rightwristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
